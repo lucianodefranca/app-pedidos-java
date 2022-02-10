be30da4866d7670bde200834814e095d6f5f4c43
luciano de franca <lucianodefranca86@gmail.com>
2022-02-10 20:33:25 -0300

    Projeto criado

---
+++ src/main/java/com/luciano/apppedidos/AppPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.luciano.apppedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luciano/apppedidos/AppPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.apppedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
