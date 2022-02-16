2128f283806586546d4d1ef1d2640671ad3ffcee
luciano de franca <lucianodefranca86@gmail.com>
2022-02-15 22:29:49 -0300

    Personalizando o retorno de validacao

--- src/main/java/com/luciano/apppedidos/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/luciano/apppedidos/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.luciano.apppedidos.services.exceptions.DataIntegrityException;
 import com.luciano.apppedidos.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,10 +18,18 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
 
-
     @ExceptionHandler(DataIntegrityException.class)
     public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e) {
         StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e) {
+        ValidationError err = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Erro de validação");
+        for (FieldError x: e.getBindingResult().getFieldErrors()) {
+            err.addError(x.getField(), x.getDefaultMessage());
+        }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
